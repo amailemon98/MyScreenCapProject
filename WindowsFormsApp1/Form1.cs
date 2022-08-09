be97db5c2cde143cbedcbb47352e903c08d06b37
Junhyung <amailemon98@naver.com>
2022-08-09 16:01:27 +0900

    초기 커밋.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        Bitmap btnbase;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSC_Click(object sender, EventArgs e)
+        {
+            btnbase = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            using(Graphics g = Graphics.FromImage(btnbase))
+            {
+                g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X, Screen.PrimaryScreen.Bounds.Y,
+                    0, 0,
+                    btnbase.Size,
+                    CopyPixelOperation.SourceCopy);
+                PicBox1.Image = btnbase;
+            }
+        }
+
+        private void BtnForSave_Click(object sender, EventArgs e)
+        {
+            if(btnbase != null)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+
+                sfd.Filter = "JPG File(*.jpg) | *.jpg";
+
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    btnbase.Save(sfd.FileName);
+                }
+            }
+        }
+    }
+}
